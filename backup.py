d283141e3183d012165904c8306939887a15f682
Bastian Kuhn <bk@mathias-kettner.de>
2013-02-27 17:46:46 +0100

    Now all notes a saved, including attachments

--- backup.py
+++ backup.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python
 import os
+import sys
 
 from config import *
+try:
+    from config_bk import *
+except:
+    pass
 
 import hashlib
 import binascii
-import evernote.edam.userstore.constants as UserStoreConstants
-import evernote.edam.type.ttypes as Types
-from evernote.edam.notestore import NoteStore
-from evernote.api.client import EvernoteClient
+try:
+    import evernote.edam.userstore.constants as UserStoreConstants
+    import evernote.edam.type.ttypes as Types
+    from evernote.edam.notestore import NoteStore
+    from evernote.api.client import EvernoteClient
+except:
+    print "Please install the evernote Python API"
+    sys.exit(1)
 
+if cfg_evernote_auth_token == "":
+    print "Please fill in the Evernote Auth Token first (config.py)"
+    sys.exit(1)
 
 def clean_filename(filename):
     chars = '-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     return ''.join(c for c in filename if c in chars)
 
 def backup_evernote():
+    if verbose:
+        print "Backup Evernote"
     remote_notes = evernote_get_remote_notes()
     local_notes = evernote_get_local_notes('evernote/notebooks')
     evernote_sync_to_local(remote=remote_notes, local=local_notes)
-    return True
+    if verbose:
+        print "============ Evernote finish ============"
 
 """
 Get all remote notes from evernote
@@ -30,22 +45,38 @@ def evernote_get_remote_notes():
     global note_store
     note_store = client.get_note_store()
     note_list = {} 
+    if verbose:
+        print "Getting Notebooks with list of notes"
     for notebook in note_store.listNotebooks():
         guid = notebook.guid
         nbname = notebook.name
+        if verbose:
+            print " - Found Notebook %s " % nbname
         note_list[nbname] = []
         filter = NoteStore.NoteFilter()
         filter.notebookGuid = guid
         # get a list of all nodes
         # Not finished
-        noteList = note_store.findNotes(cfg_evernote_auth_token,filter,0,12)
-
-        for n in noteList.notes:
-            note_list[nbname].append((n.guid, { "title"         : n.title,
-                                                "updated"       : n.updated,
-                                                "content_hash"  : n.contentHash,
-                                                }))
         
+        notes = 1
+        start = 0
+        stop = 50
+        while notes > 0:
+            if debug:
+                print " --  %s Notes from %d to %d (Notes %d)" % (nbname, start, stop, notes)
+            noteList = note_store.findNotes(cfg_evernote_auth_token,filter,start,stop)
+            start += 51
+            stop += 100
+            notes = len(noteList.notes)
+
+            for n in noteList.notes:
+                note_list[nbname].append((n.guid, { "title"         : n.title,
+                                            "updated"       : n.updated,
+                                            "content_hash"  : n.contentHash,
+                                            }))
+        if verbose:
+            print " -- Found %s entries in %s" % (len(note_list[nbname]), nbname)
+            
     return note_list 
 
 """Get all local saved notes """
@@ -61,15 +92,16 @@ def evernote_get_local_notes(what):
 
 """Sync all notes to local"""
 def evernote_sync_to_local(remote, local):
+    if verbose:
+        print "Now Syncing the Notes from Evernote to local"
     path = cfg_backup_base_url + "/evernote/notebooks"
     local_missing = {}
-
     for notebook, remote_notes in remote.items():
         local_notes = local.get(notebook) 
         #Check if complete notebook is missing
         if local_notes == None:
             if verbose:
-                print "Evernote: New notebook %s" % notebook
+                print " - Theres a new one: %s (Sync complete)" % notebook
             local_missing[notebook] = remote_notes
             continue
         local_notes = dict(local_notes)
@@ -77,18 +109,20 @@ def evernote_sync_to_local(remote, local):
             #Check for missing notes in notebook
             if remote_note_id not in local_notes.keys():
                 if verbose:
-                    print "Evernote: New Note: %s" % remote_note_data['title']
+                    print " - A new note: %s" % remote_note_data['title']
                 local_missing.setdefault(notebook, [])
                 local_missing[notebook].append((remote_note_id, remote_note_data))
                 continue
             #Check for Changed notes
             if local_notes[remote_note_id]['updated'] != remote_note_data['updated']:
                 if verbose:
-                    print "Evernote: Changed Note: %s" % remote_note_data['title']
+                    print " - A changed note: %s" % remote_note_data['title']
                 local_missing.setdefault(notebook, [])
                 local_missing[notebook].append((remote_note_id, remote_note_data))
                 continue
-     
+    
+    if verbose:
+        print "Download Notes"
     for notebook, notebook_data in local_missing.items():
         try:
             save_path = path + "/" + notebook
@@ -96,21 +130,46 @@ def evernote_sync_to_local(remote, local):
         except os.error:
             pass
         if verbose:
-            print "Evernote: Syncing Notebook %s: " % notebook
+            print " - Notebook is %s: " % notebook
  
         for save_note_id, save_note_data in dict(notebook_data).items():
             if verbose:
-                print "          -- %s" % save_note_data['title'] 
+                print " -- %s" % save_note_data['title'] 
                   # bool withContent,
                   # bool withResourcesData,
                   # bool withResourcesRecognition,
                   # bool withResourcesAlternateData)
-            note = note_store.getNote(cfg_evernote_auth_token, save_note_id, True, False, False, False)
+            note = note_store.getNote(cfg_evernote_auth_token, save_note_id, True, True, True, True)
+            #Saving Content
             file(save_path + "/" + clean_filename(save_note_data['title']), "w").write(str(note.content))
+            if cfg_evernote_dump:
+                import inspect
+                file(save_path + "/" + clean_filename(save_note_data['title'] + ".dump"), "w")\
+                .write(str(inspect.getmembers(note)))
+
+            #Saving Attachments
+            files = {}
+            if note.resources:
+                for res in note.resources:
+                    files[res.guid] = {}
+                    files[res.guid]['mime'] = res.mime
+                    files[res.guid]['fileName'] = res.attributes.fileName
+                    if res.attributes.fileName != None:
+                        att_path = save_path + "/" + clean_filename(save_note_data['title'])+"-files"
+                        try:
+                            os.makedirs(att_path)
+                        except os.error:
+                            pass
+                        file(att_path + "/" + clean_filename(res.attributes.fileName), "w").write(res.data.body)
+                #print files
 
         #Saving the stat file
         file(path+"/stats","w").write(str(remote))
     
+        if verbose:
+            print "Move deleted Notes to trash"
+
+
 
 
 backup_evernote()
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 verbose = True
+debug = False
 cfg_backup_base_url = "/home/..."
 
 #Evernote Settings
 cfg_evernote_auth_token = ""
 cfg_evernote_sandbox = False 
+cfg_evernote_dump = False #Create a full inspect dump of each note
